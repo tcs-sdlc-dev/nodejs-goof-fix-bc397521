c154765def70f8bb99bfb3d3d613545fc50f82af
Ansh Roshan <anshroshan813210@gmail.com>
2026-05-13 09:21:52 +0530

    fix(security): apply automated fixes for 99 findings across 6 files
    Scan ID: 572bbb01-a8df-4574-a1df-1b24b38eab8a
    Source: https://github.com/tcs-sdlc-dev/nodejs-goof-fix-bc397521
    Files: package.json, app.js, docker-compose.yml, views/admin.ejs, views/index.ejs, Dockerfile

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var st = require('st');
 var crypto = require('crypto');
 var express = require('express');
 var http = require('http');
+var https = require('https');
+var fs = require('fs');
+
 var path = require('path');
 var ejsEngine = require('ejs-locals');
 var bodyParser = require('body-parser');
@@ -25,7 +28,12 @@ var dustHelpers = require('dustjs-helpers');
 var cons = require('consolidate');
 const hbs = require('hbs')
 
+if (!process.env.SESSION_SECRET || process.env.SESSION_SECRET.length < 32) {
+  throw new Error('SESSION_SECRET environment variable must be set to a random string of at least 32 characters.');
+}
+
 var app = express();
+
 var routes = require('./routes');
 var routesUsers = require('./routes/users.js')
 
@@ -39,14 +47,34 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(methodOverride());
+app.set('trust proxy', 1);
 app.use(session({
-  secret: 'keyboard cat',
+  secret: process.env.SESSION_SECRET,
   name: 'connect.sid',
-  cookie: { path: '/' }
+  resave: false,
+  saveUninitialized: false,
+  rolling: true,
+  cookie: {
+    path: '/',
+    httpOnly: true,
+    sameSite: 'strict',
+    secure: process.env.NODE_ENV === 'production',
+    maxAge: 30 * 60 * 1000,
+    domain: process.env.COOKIE_DOMAIN || undefined
+  }
 }))
+
+var csrf = require('csurf');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(fileUpload());
+var csrfProtection = csrf();
+app.use(csrfProtection);
+app.use(function(req, res, next) {
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
 
 // Routes
 app.use(routes.current_user);
@@ -58,7 +86,8 @@ app.get('/account_details', routes.isLoggedIn, routes.get_account_details);
 app.post('/account_details', routes.isLoggedIn, routes.save_account_details);
 app.get('/logout', routes.logout);
 app.post('/create', routes.create);
-app.get('/destroy/:id', routes.destroy);
+app.post('/destroy/:id', routes.destroy);
+
 app.get('/edit/:id', routes.edit);
 app.post('/update/:id', routes.update);
 app.post('/import', routes.import);
@@ -80,9 +109,27 @@ if (app.get('env') == 'development') {
   app.use(errorHandler());
 }
 
-var token = 'SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9';
-console.log('token: ' + token);
-
-http.createServer(app).listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
+app.use(function(err, req, res, next) {
+  if (err.code === 'EBADCSRFTOKEN') {
+    return res.status(403).send('Invalid or missing CSRF token.');
+  }
+  next(err);
 });
+
+
+var token = process.env.SECRET_TOKEN;
+
+var tlsOptions = {
+  key: fs.readFileSync(process.env.TLS_KEY_PATH),
+  cert: fs.readFileSync(process.env.TLS_CERT_PATH)
+};
+
+https.createServer(tlsOptions, app).listen(app.get('port'), function () {
+  console.log('Express HTTPS server listening on port ' + app.get('port'));
+});
+
+http.createServer(function (req, res) {
+  res.writeHead(301, { Location: 'https://' + req.headers.host + req.url });
+  res.end();
+}).listen(80);
+
